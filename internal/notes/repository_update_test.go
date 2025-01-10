8d5e553dbad3d6ca05f7b85fa6d136290952d02e
Martin Remøy Solheim <martin.solheim@me.com>
2025-01-10 19:40:24 +0100

    Refactor update note test

--- internal/notes/repository_update_test.go
+++ internal/notes/repository_update_test.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-func TestRepository_UpdateNote_Success(t *testing.T) {
+func TestRepository_UpdateNote(t *testing.T) {
 	db, err := sql.Open("sqlite3", ":memory:")
 	require.NoError(t, err)
 	defer db.Close()
@@ -21,41 +21,34 @@ func TestRepository_UpdateNote_Success(t *testing.T) {
 
 	repo := notes.NewRepository(db)
 
-	originalNote, err := repo.CreateNote(context.Background(), "Original text")
-	require.NoError(t, err)
-	require.NotEmpty(t, originalNote.ID)
+	t.Run("success", func(t *testing.T) {
+		originalNote, err := repo.CreateNote(context.Background(), "Original text")
+		require.NoError(t, err)
+		require.NotEmpty(t, originalNote.ID)
 
-	updatedText := "Updated text"
-	updatedNote, err := repo.UpdateNote(context.Background(), originalNote.ID, updatedText)
-	require.NoError(t, err, "Expected successful update, got error instead")
+		updatedText := "Updated text"
+		updatedNote, err := repo.UpdateNote(context.Background(), originalNote.ID, updatedText)
+		require.NoError(t, err, "Expected successful update, got error instead")
 
-	require.Equal(t, originalNote.ID, updatedNote.ID, "ID should be unchanged")
-	require.Equal(t, updatedText, updatedNote.Text, "Text should be updated")
+		require.Equal(t, originalNote.ID, updatedNote.ID, "ID should be unchanged")
+		require.Equal(t, updatedText, updatedNote.Text, "Text should be updated")
 
-	require.WithinDuration(t, originalNote.Created, updatedNote.Created, time.Second, "Created timestamp should be the same")
+		require.WithinDuration(t, originalNote.Created, updatedNote.Created, time.Second, "Created timestamp should be the same")
 
-	fetchedAll, err := repo.GetAllNotes(context.Background())
-	require.NoError(t, err)
-	require.Len(t, fetchedAll, 1)
-	require.Equal(t, updatedText, fetchedAll[0].Text, "Text in the DB should be updated")
-}
+		fetchedAll, err := repo.GetAllNotes(context.Background())
+		require.NoError(t, err)
+		require.Len(t, fetchedAll, 1)
+		require.Equal(t, updatedText, fetchedAll[0].Text, "Text in the DB should be updated")
+	})
 
-func TestRepository_UpdateNote_NotFound(t *testing.T) {
-	db, err := sql.Open("sqlite3", ":memory:")
-	require.NoError(t, err)
-	defer db.Close()
+	t.Run("not found", func(t *testing.T) {
+		originalNote, err := repo.CreateNote(context.Background(), "Original text")
+		require.NoError(t, err)
+		require.NotEmpty(t, originalNote.ID)
 
-	err = db2.RunMigrations(db, "file://../../migrations")
-	require.NoError(t, err)
-
-	repo := notes.NewRepository(db)
-
-	originalNote, err := repo.CreateNote(context.Background(), "Original text")
-	require.NoError(t, err)
-	require.NotEmpty(t, originalNote.ID)
-
-	updatedText := "Updated text"
-	_, err = repo.UpdateNote(context.Background(), "not an id", updatedText)
-	require.NotEmpty(t, err)
-	require.Equal(t, err, notes.ErrNoteNotFound)
+		updatedText := "Updated text"
+		_, err = repo.UpdateNote(context.Background(), "not an id", updatedText)
+		require.NotEmpty(t, err)
+		require.Equal(t, err, notes.ErrNoteNotFound)
+	})
 }
